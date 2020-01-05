d1b097291de96ca15cc291e1c8f54cd27ae1e088
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-01-05 20:40:56 +0100

    jeg har lavet meget

--- script.js
+++ script.js
@@ -27,3 +27,41 @@ document.querySelector("#port_btn").addEventListener("click", function () {
 
     }
 })
+
+
+let slideIndex = 0;
+//showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let slideContainer = document.querySelector(".slides");
+    if (n > slides.length - 1) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = n;
+    }
+
+    slideContainer.className = "slides";
+    slideContainer.classList.add("slide" + slideIndex);
+}
+
+
+let colapse_btn = document.querySelectorAll(".colapse_btn");
+
+for (let index = 0; index <= colapse_btn.length - 1; index++) {
+    colapse_btn[index].addEventListener("click", function () {
+        this.classList.toggle("open");
+    });
+}
